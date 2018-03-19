4c6866870b3cb0eb7867c2a7c55f0e38a7e08c67
Jim Gisler <jim.gisler@gmail.com>
2018-03-18 20:29:20 -0700

    added putObject

--- scheduled-chained-lambda/src/lib/QueueProcessor.js
+++ scheduled-chained-lambda/src/lib/QueueProcessor.js
@@ -4,7 +4,7 @@
 class QueueProcessor {
 
    static getInstance(s3Client, sqsClient) {
-      if(theInstance === undefined) {
+      if (theInstance === undefined) {
          theInstance = new QueueProcessor(s3Client, sqsClient);
       }
       return theInstance;
@@ -12,18 +12,21 @@ class QueueProcessor {
 
    constructor(s3Client, sqsClient) {
       this.class = this.constructor.name;
+      
       this.s3Client = s3Client;
       this.sqsClient = sqsClient;
+
       this.waitTimeInSeconds = process.env.ReceiveMessageWaitTimeInSeconds;
       this.messageVisibilityTimeoutInSeconds = process.env.MessageVisibilityTimeoutInSeconds;
       this.maxNumberOfMessages = process.env.MaxNumberOfMessages;
+      this.messageBucket = process.env.MessageBucket;
    }
 
    processMessages(queueUrl) {
 
       return this.receiveMessages(queueUrl)
          .then((result) => this.persistMessages(result.Messages))
-         .then((result) => this.deleteMessages(queueUrl, result));
+         .then((result) => this.deleteMessages(result, queueUrl));
 
    }
 
@@ -37,9 +40,24 @@ class QueueProcessor {
    }
 
    persistMessages(messages) {
+      const putPromises = [];
       messages.forEach((message) => {
-         
+         messageFiles.push(this.putObject(message));
       });
+      return Promise.all(putPromises);
+   }
+
+   putObject(message) {
+      return this.s3Client.putObject({
+         Bucket: this.messageBucket,
+         Key: `${message.MessageId}.json`,
+         ContentType: 'application/json',
+         Body: JSON.stringify(message.Body)
+      }).promise();
+   }
+
+   deleteMessages(message, queueUrl) {
+
    }
 
 }
